95633402c1d7bbe150af22cd92fa938ad39082c1
ddeadmond <ddeadmond@bps.k12.in.us>
2016-10-10 10:39:45 -0400

    Update_Team_4_save1

--- team4.py
+++ team4.py
@@ -1,14 +1,14 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
+#     team_4: a string
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_4 = 'team 4' # Only 10 chars displayed.
+strategy_name = 'Process of elimination'
+strategy_description = 'I will either betray or collude with them'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
